e73ba99b7e09234e91da91297980ecf6191cef6e
helahmad <ahmadmhelmi@gmail.com>
2023-03-23 16:44:41 -0400

    block07 added

---
+++ block07/vault.js
@@ -0,0 +1,22 @@
+/* 
+Psudocode
+1- Create a String message to welcome the user and explain the purpose
+    of this message.
+2- generate 3 numbers using different arrithmetic operators which will
+    represent the 3 codes for opening the vault.
+3- store each number in a separate variable.
+4- display the combination in an alert pop-up.
+
+*/ 
+
+//Create a string that welcomes the user and explains the purpose of the message.
+alert("hello, you have been chosen to open an important vault, here is the secret combination code:");
+
+//Generate three numbers using different arithmetic operators and Store each in a variable.
+let code1 = 20 / 2 ;
+const code2 = 100 % 60 ;
+var code3 = 40 - 1 ;
+
+//Display the combination in an alert popup
+alert(`${code1} - ${code2} - ${code3}`);
+
